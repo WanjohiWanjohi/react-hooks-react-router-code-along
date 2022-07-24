927cacd4e9033350a80046b517a83485a57fbb82
WanjohiWanjohi <wamuyuwanjohi97@gmail.com>
2022-07-24 20:35:49 +0300

    commit tree 462e9c76db44dfd49a74bdc2e816fd5fd8881dda

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Routes, Switch } from "react-router-dom";
 
 function Home() {
   return (
     <div>
       <h1>Home!</h1>
+    </div>
+  );
+}
+function About() {
+  return (
+    <div>
+      <h1>This is my about component!</h1>
     </div>
   );
 }
@@ -14,3 +22,13 @@ function App() {
 }
 
 ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <BrowserRouter>
+    <Routes>
+       <Route path='/' element={<Home/>} />
+       <Route path='/about' element={<About/>} />
+  
+    </Routes>
+  </BrowserRouter>,
+  document.getElementById('root')
+);
